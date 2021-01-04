ead5326633481c60013bc7217605e0a2f29b4ce5
jas4gan <jas4gan@gmail.com>
2021-01-04 22:34:16 +0800

    routes working with 5000/users and /users/bwyd

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
+const bodyParser = require("body-parser");
+const db = require("../model/helper");
+
+router.use(bodyParser.json());
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+  res.send('Welcome to Bwyd database');
+});
+
+router.get("/bwyd", (req, res) => {
+  db("SELECT * FROM users ORDER BY userid ASC;SELECT * FROM items ORDER BY itemid ASC;SELECT * FROM orders ORDER BY orderid ASC;")
+    .then(
+      results => res.send(results.data))
+    .catch(res => res.status(500).send(err));
+
+});
+
+router.post("/bwyd", (req, res) => {
+  db(
+    `INSERT INTO bwyd (name, ) VALUES (${JSON.stringify(
+      req.body.name
+    )},${req.body.complete}) `
+  ).then(results => {
+    const data = {
+      id: results.insertId,
+      text: req.body.text,
+      complete: req.body.complete
+    };
+    return res.send(data);
+  });
 });
 
 module.exports = router;
